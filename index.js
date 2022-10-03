9225bcae8f6ada486563ccded48c8621a205720d
NMN03 <nmnkhera@gmail.com>
2022-10-03 19:00:46 +0530

    updated the guitar section

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
+$(".hotel").click(handler1);
+function handler1(){
+  $(".btn").prop("disabled",true);
+  var sound="audio/hotel_california.aac";
+  var audio= new Audio(sound);
+  audio.play();
+  setTimeout(function(){
+    $(".btn").prop("disabled",false);
+  },57000)
+}
 
+$(".godfather").click(handler2);
+function handler2(){
+  $(".btn").prop("disabled",true);
+  var sound="audio/godfather.aac";
+  var audio= new Audio(sound);
+  audio.play();
+  setTimeout(function(){
+    $(".btn").prop("disabled",false);
+  },51000)
+}
+$(".pink").click(handler3);
+function handler3(){
+  $(".btn").prop("disabled",true);
+  var sound="audio/pink_panther.aac";
+  var audio= new Audio(sound);
+  audio.play();
+  setTimeout(function(){
+    $(".btn").prop("disabled",false);
+  },45000)
+}
+$(".pirates").click(handler4);
+function handler4(){
+  $(".btn").prop("disabled",true);
+  var sound="audio/pirates.aac";
+  var audio= new Audio(sound);
+  audio.play();
+  setTimeout(function(){
+    $(".btn").prop("disabled",false);
+  },43000)
+}
+$(".symphony").click(handler5);
+function handler5(){
+  $(".btn").prop("disabled",true);
+  var sound="audio/symphony.aac";
+  var audio= new Audio(sound);
+  audio.play();
+  setTimeout(function(){
+    $(".btn").prop("disabled",false);
+  },18000)
+}
